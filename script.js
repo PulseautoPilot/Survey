e06da81f9fed16816e604f0f928802fee8958c0b
PulseautoPilot <154662074+PulseautoPilot@users.noreply.github.com>
2023-12-28 14:20:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const submitBtnA = document.getElementById('submitBtnA');
-    const submitBtnB = document.getElementById('submitBtnB');
-    const sessionCodeSpan = document.getElementById('sessionCode');
+window.onload = function() {
+    // Generate a unique alpha-numeric code
+    var sessionCode = Math.random().toString(36).substr(2, 5);
+    document.getElementById('sessionCode').innerHTML = 'Session Code: ' + sessionCode;
 
-    // Function to generate a unique session code
-    function generateSessionCode() {
-        return Math.random().toString(36).substring(2, 10);
-    }
+    // Hide submit button for 1 minute after moving to Section 2
+    setTimeout(function() {
+        document.getElementById('submit1').style.display = 'block';
+    }, 60000);
 
-    // Event listener for Section A submit button
-    submitBtnA.addEventListener('click', function () {
-        document.getElementById('companyReview').classList.add('hidden');
-        document.getElementById('socialSharing').classList.remove('hidden');
+    // Hide submit button for 1 minute after moving to Section 3
+    document.getElementById('submit1').onclick = function() {
+        document.getElementById('section1').style.display = 'none';
+        document.getElementById('section2').style.display = 'block';
+        setTimeout(function() {
+            document.getElementById('submit2').style.display = 'block';
+        }, 60000);
+    };
 
-        // Start timer for Section B
-        setTimeout(function () {
-            submitBtnB.disabled = false;
-        }, 60000); // 1 minute delay
-
-        // Display session code
-        const sessionCode = generateSessionCode();
-        sessionCodeSpan.textContent = sessionCode;
-    });
-
-    // Event listener for Section B submit button
-    submitBtnB.addEventListener('click', function () {
-        document.getElementById('socialSharing').classList.add('hidden');
-        document.getElementById('completion').classList.remove('hidden');
-    });
-});
+    document.getElementById('submit2').onclick = function() {
+        document.getElementById('section2').style.display = 'none';
+        document.getElementById('section3').style.display = 'block';
+    };
+};
